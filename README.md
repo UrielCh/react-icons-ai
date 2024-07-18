6439d1352e1429e76d502cf8b84f363eb363351c
urielch <uriel.chemouni@gmail.com>
2024-07-18 10:53:02 +0200

    update doc

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 
 [See available icons here](https://react-icons.deno.dev/ai)
 
-## install packahe
+## install the module
 
 ```bash
 deno add @preact-icons/ai
@@ -17,36 +17,15 @@ pnpm dlx jsr add @preact-icons/ai
 bunx jsr add @preact-icons/ai
 ```
 
-You may need to update your preact mapping to:
- - "preact": "npm:preact@10.22.1",
- - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- - "preact/hooks": "npm:preact@10.22.1/hooks",
-
-
-# Ant Design Icons icons for preact
-
-[![JSR](https://jsr.io/badges/@preact-icons/ai)](https://jsr.io/@preact-icons/ai)
-
-**License** [MIT](https://opensource.org/licenses/MIT)
-
-**Project** [https://github.com/ant-design/ant-design-icons](https://github.com/ant-design/ant-design-icons)
-
-[See available icons here](https://react-icons.deno.dev/ai)
-
-## install packahe
-
-```bash
-deno add @preact-icons/ai
-dnpx jsr add @preact-icons/ai
-pnpm dlx jsr add @preact-icons/ai
-bunx jsr add @preact-icons/ai
+You may need to update your preact mapping to avoid mixing JSR and http import:
+Currently Deno fresh import preact using https://esm.sh/preact http import can not be mixed with JSR package, so you may need to update your preact mapping:
+```json
+{
+ "preact": "npm:preact@10.22.1",
+ "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ "preact/hooks": "npm:preact@10.22.1/hooks",
+}
 ```
-
-You may need to update your preact mapping to:
- - "preact": "npm:preact@10.22.1",
- - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- - "preact/hooks": "npm:preact@10.22.1/hooks",
-
 
 ## import an icon from all icons
 
@@ -65,4 +44,3 @@ or using default export
 ```ts
 import AiFillAccountBook from "react-icons/ai/AiFillAccountBook.ts"
 ```
-
--- mod.ts
+++ mod.ts
@@ -10,7 +10,7 @@
  * 
  * [See available icons here](https://react-icons.deno.dev/ai)
  * 
- * ## install packahe
+ * ## install the module
  * 
  * ```bash
  * deno add @preact-icons/ai
@@ -19,36 +19,15 @@
  * bunx jsr add @preact-icons/ai
  * ```
  * 
- * You may need to update your preact mapping to:
- *  - "preact": "npm:preact@10.22.1",
- *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *  - "preact/hooks": "npm:preact@10.22.1/hooks",
- * 
- * 
- * # Ant Design Icons icons for preact
- * 
- * [![JSR](https://jsr.io/badges/@preact-icons/ai)](https://jsr.io/@preact-icons/ai)
- * 
- * **License** [MIT](https://opensource.org/licenses/MIT)
- * 
- * **Project** [https://github.com/ant-design/ant-design-icons](https://github.com/ant-design/ant-design-icons)
- * 
- * [See available icons here](https://react-icons.deno.dev/ai)
- * 
- * ## install packahe
- * 
- * ```bash
- * deno add @preact-icons/ai
- * dnpx jsr add @preact-icons/ai
- * pnpm dlx jsr add @preact-icons/ai
- * bunx jsr add @preact-icons/ai
+ * You may need to update your preact mapping to avoid mixing JSR and http import:
+ * Currently Deno fresh import preact using https://esm.sh/preact http import can not be mixed with JSR package, so you may need to update your preact mapping:
+ * ```json
+ * {
+ *  "preact": "npm:preact@10.22.1",
+ *  "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ *  "preact/hooks": "npm:preact@10.22.1/hooks",
+ * }
  * ```
- * 
- * You may need to update your preact mapping to:
- *  - "preact": "npm:preact@10.22.1",
- *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *  - "preact/hooks": "npm:preact@10.22.1/hooks",
- * 
  * 
  * ## import an icon from all icons
  * 
@@ -67,8 +46,8 @@
  * ```ts
  * import AiFillAccountBook from "react-icons/ai/AiFillAccountBook.ts"
  * ```
- * 
  * @module
+ * 
  */
 
 import { GenIcon } from "./deps.ts";
